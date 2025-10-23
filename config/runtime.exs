d3b728f0713cd383283dc80d95772ed83111b925
De Wet Blomerus <dewetblomerus@gmail.com>
2025-10-22 22:10:16 -0400

    fly launch

--- config/runtime.exs
+++ config/runtime.exs
@@ -27,11 +27,7 @@ end
 
 if config_env() == :prod do
   database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+    System.fetch_env!("DATABASE_URL")
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\mobile_worship" eval MobileWorship.Release.migrate
