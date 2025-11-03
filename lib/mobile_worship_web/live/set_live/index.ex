3053c3a1b82128586d66fcf6e865c1beaefce67c
De Wet Blomerus <dewetblomerus@gmail.com>
2025-11-02 21:39:15 -0500

    Present straight from sets index

--- lib/mobile_worship_web/live/set_live/index.ex
+++ lib/mobile_worship_web/live/set_live/index.ex
@@ -28,6 +28,9 @@ defmodule MobileWorshipWeb.SetLive.Index do
           <div class="sr-only">
             <.link navigate={~p"/sets/#{set}"}>Show</.link>
           </div>
+          <.link navigate={~p"/sets/#{set.id}/present"} class="text-primary flex items-center gap-1 whitespace-nowrap">
+            <.icon name="hero-presentation-chart-bar" /> Present
+          </.link>
           <.link navigate={~p"/sets/#{set}/edit"}>Edit</.link>
         </:action>
         <:action :let={{id, set}}>
